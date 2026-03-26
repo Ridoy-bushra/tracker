83a0e10817d9049c310bef73ef9b294b3a394abf
Ridoy-bushra <bushrabushra2710@gmail.com>
2026-03-26 21:58:13 +0600

    nine

---
+++ script/trac.js
@@ -0,0 +1,12 @@
+let currentTab = "all";
+const tabActive = ["btn-info", "text-white"];
+const tabInactive = ["btn-nutral", "text-black"];
+
+const allcontainer = selectorClass("job-card-container");
+const interviewcontainer = selectorId("interview-section");
+const rejectcontainer = selectorId("reject-section");
+const interviewCount = selectorId("interview-count");
+const rejectCount = selectorId("reject-count");
+const totaljobCount = selectorId("total-job");
+const toShowOutofCount = selectorId("toShowOutof");
+const noJob = selectorId("nojob-popup");
